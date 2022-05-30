28ce9750805f919bd3920b9e51dbd16fc4aeee81
luciomelis <luciomelis1992@gmail.com>
2022-05-30 20:18:14 +0200

    modifiche

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Str;
+
 use App\Post;
 
 class PostController extends Controller
