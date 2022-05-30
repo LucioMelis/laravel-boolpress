7829055ff8f01ee48a31ae0e2a0828cbc0b02b02
luciomelis <luciomelis1992@gmail.com>
2022-05-30 19:14:52 +0200

    modifiche sul model

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'posts';
     protected $fillable = ['title', 'content', 'slug'];
 }
