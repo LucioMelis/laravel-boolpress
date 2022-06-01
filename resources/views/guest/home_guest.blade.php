b54b06097a29f621a12de69cdb99e223c7d1cac3
luciomelis <luciomelis1992@gmail.com>
2022-06-01 15:36:44 +0200

    Esercizio Terminato con modifica sulla Guest

--- resources/views/guest/home_guest.blade.php
+++ resources/views/guest/home_guest.blade.php
@@ -1,1 +1,4 @@
-<h1>Non sei connesso!</h1>
+<h1>Benvenuto su Boolpress</h1>
+
+<p>ACCEDI: <a href="{{ route('login') }}">Login</a></p>
+<p>NUOVO UTENTE: <a href="{{ route('register') }}">Registrati</a></p>
