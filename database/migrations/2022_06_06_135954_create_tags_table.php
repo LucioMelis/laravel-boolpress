3c882e881524a1b99f62674789e6b88fcdb409db
luciomelis <luciomelis1992@gmail.com>
2022-06-06 16:45:40 +0200

    creata la tabella pivot

--- database/migrations/2022_06_06_135954_create_tags_table.php
+++ database/migrations/2022_06_06_135954_create_tags_table.php
@@ -15,6 +15,8 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name',50);
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
---
+++ database/migrations/2022_06_06_143852_create_post_tag_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            // Assegnazione colonna post_id
+            $table->unsignedBigInteger('post_id');
+            $table->foreign('post_id')
+            ->references('id')
+            ->on('posts');
+            // Assegnazione colonna tag_id
+            $table->unsignedBigInteger('tag_id');
+            $table->foreign('tag_id')
+            ->references('id')
+            ->on('tags');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+}
