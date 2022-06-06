50b828be1ce5f01051426381f11ff55e7e3cf33f
luciomelis <luciomelis1992@gmail.com>
2022-06-06 20:20:36 +0200

    modifica sullo show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -16,11 +16,25 @@
         </form>
     </div>
     <div class="mt-3">
-        <h3>{{ $post->title }}</h3>
+        <h3>Titolo</h3>
+        <p>{{ $post->title }}</p>
+        <h3>Slug</h3>
+        <p>{{ $post->slug }}</p>
+        <h3>Contenuto</h3>
         <p>{{ $post->content }}</p>
         {{-- $post viene mandato dallo show, category è la funzione presente nel modello post, name è l'attributo presente nella colonna della tabella category --}}
         {{-- questo perchè ci sono delle relazioni tra le tabelle --}}
+        <h3>Categoria</h3>
         <p>{{ $post->category->name }}</p>
+        <h3>Tags</h3>
+        {{-- @foreach ($post->tags as $tag)
+            <span>{{ $tag->name }}</span>
+        @endforeach --}}
+        @forelse ($post->tags as $tag)
+            <p>{{ $tag->name }}</p>
+        @empty
+            <p>Not found</p>
+        @endforelse
         <button class="btn btn-success">
             <a href="{{ route('admin.posts.index') }}">Torna ai Post</a>
         </button>
