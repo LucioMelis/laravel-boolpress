237f4fce711d33e6b7c9fc390a93c1e623bba6f2
luciomelis <luciomelis1992@gmail.com>
2022-06-09 09:34:01 +0200

    ok js

---
+++ resources/views/layouts/front-app.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="{{ strreplace('', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/front-app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.18/vue.min.js%22%3E"></script>
+</head>
+
+<body>
+    <div id=" app">
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+</body>
+
+</html>
