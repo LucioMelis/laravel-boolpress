7ad54deb7c6bd0402e1fd32e7d3ebd60f9a45325
luciomelis <luciomelis1992@gmail.com>
2022-06-09 09:58:30 +0200

    aggiunta estensione ma errori sul browser

--- resources/views/guest/home_guest.blade.php
+++ resources/views/guest/home_guest.blade.php
@@ -3,7 +3,6 @@
 
 @section('vue')
     <h1>Benvenuto su Boolpress</h1>
-
     <p>ACCEDI: <a href="{{ route('login') }}">Login</a></p>
     <p>NUOVO UTENTE: <a href="{{ route('register') }}">Registrati</a></p>
 @endsection
