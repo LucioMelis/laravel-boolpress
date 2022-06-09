fbaa276db497260d4457eeb2942a79231372361e
luciomelis <luciomelis1992@gmail.com>
2022-06-09 09:40:32 +0200

    aggiunta stringa prova per axios

--- resources/js/front-app.js
+++ resources/js/front-app.js
@@ -2,6 +2,8 @@ console.log("Ok Js:)");
 require("./bootstrap");
 
 window.axios = require("axios");
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 // window.Vue = require("vue");
 window.axios
     .get("/api/posts")
